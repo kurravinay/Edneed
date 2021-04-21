dd21a6a083b4db1a3bdc47fbbd9a5ad10263e83d
kurravinay <vinay.kurra@gmail.com>
2021-04-21 12:20:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,141 @@
+const express = require("express");
+const app = express();
+const multer = require('multer'); // extension To Upload files
+const path = require('path');
+const dotenv = require("dotenv"); // To Maintain Config files
+dotenv.config();
+const mongoose = require("mongoose"); // Extension to perform oprations on Mongo
+
+app.use(express.static('files'))
+const Task = require("./models/Task");
+const upload = multer({dest:  '/uploads/'});
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+const helpers = require('./models/helpers');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null,'/public/uploads/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+//Connection to  Mongo db
+mongoose.set("useFindAndModify", false);
+mongoose.connect(process.env.DB_CONNECT, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }, () => {
+console.log("Connected to db!");
+});
+
+// setting up the view engine
+app.set("view engine", "ejs");
+
+// Default route
+app.get('/',(req, res) => {
+
+ Task.find({}, (err, parent) =>
+ {
+ //console.log(parent);
+res.render("task.ejs", { Tasks: parent });
+});
+});
+
+// To Edit the the list item
+app
+.route("/edit/:id")
+.get((req, res) => {
+const id = req.params.id;
+
+Task.find({}, (err, tasks) => {
+res.render("edit.ejs", { edneedTasks: tasks, idTask: id });
+});
+})
+.post((req, res) => {
+const id = req.params.id;
+let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('cicon');
+
+  upload(req, res, function(err) {
+      // req.file contains information of uploaded file
+      // req.body contains information of text fields, if there were any
+
+      if (req.fileValidationError) {
+          return res.send(req.fileValidationError);
+      }
+      else if (!req.file) {
+          return res.send('Please select an image to upload');
+      }
+      else if (err instanceof multer.MulterError) {
+          return res.send(err);
+      }
+      else if (err) {
+          return res.send(err);
+      }
+
+      // Display uploaded image for user validation
+      //res.send({req.file.path});
+     console.log(req.file)
+     Task.findByIdAndUpdate(id, { content: req.body.content ,cicon:req.file.filename}, err => {
+     if (err) return res.send(500, err);
+     res.redirect("/");
+     });
+  });
+});
+
+// To Add the the list item
+app
+.route("/add/:id")
+.get((req, res) => {
+const id = req.params.id;
+Task.find({}, (err, tasks) => {
+res.render("add.ejs", { edneedTasks: tasks, idTask: id });
+});
+})
+.post((req, res) => {
+
+const toTask = new Task({
+content: req.body.content,
+parent:req.body.parent,
+weight:req.body.weight*10
+});
+console.log("hrh");
+toTask.save();
+res.redirect("/");
+});
+
+
+app.get("/", (req, res) => {
+Task.find({}, (err, tasks) => {
+res.render("list.ejs", { Tasks: tasks });
+});
+});
+
+app.post('/',async (req, res) => {
+//quwery=Task.find({"parent":null}).sort([['weight', -1]]).limit(1).select('weight');
+//quwery.toArray()[0]["weight"];
+//console.log(quwery.toArray()[0]["weight"]);
+
+const toTask = new Task({
+content: req.body.content,
+});
+try {
+await toTask.save();
+res.redirect("/");
+} catch (err) {
+res.redirect("/goo");
+console.log(err)
+}
+});
+
+// To Remove the category from the list
+
+app.route("/remove/:id").get((req, res) => {
+const id = req.params.id;
+Task.findByIdAndRemove(id, err => {
+if (err) return res.send(500, err);
+res.redirect("/");
+});
+});
+
+app.listen(3000, () => console.log("Server Up and running"));
